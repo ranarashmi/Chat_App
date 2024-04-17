27634834a57ba54c04e1e27fc0d42242b0242eb9
rashmi.rana <rashmirana9722@gmail.com>
2024-04-17 18:21:38 +0530

    adding server and client files

---
+++ Client.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+class Client{
+
+    Socket s; 
+    BufferedReader br;
+    PrintWriter out;
+
+    public Client(){
+        try {
+            System.out.println("Sending request to server");
+            s = new Socket("127.0.0.1",7777);
+            System.out.println("Connection Done....");
+            br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            out = new PrintWriter(s.getOutputStream());
+
+            startReading();
+            startWriting();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startReading(){
+
+        Runnable r1 = ()->{
+            System.out.println("Reading started...");
+         try {
+            while (true) {
+                    String msg = br.readLine();
+                if(msg.equals("exit")){
+                    System.out.println("Client turminated the chat");
+                    break;
+                }
+
+                System.out.println("Clinet: "+msg);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+
+        new Thread(r1).start();
+
+    }
+
+    public void startWriting(){
+        Runnable r2=()->{
+            System.out.println("Writting started...");
+         try {
+            while (true) {
+                    BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+                    String content = br1.readLine();
+                    out.println(content);
+                    out.flush();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+        new Thread(r2).start();
+    }
+ 
+
+
+    public static void main(String args[]){
+        System.out.println("This is client......");
+        new Client();
+    }
+}
---
+++ Server.java
@@ -0,0 +1,77 @@
+import java.net.*;
+import java.io.*;
+
+class Server{
+
+    ServerSocket server;
+    Socket socket;
+    BufferedReader br;
+    PrintWriter out;
+
+    public Server(){
+        try {
+            server = new ServerSocket(7777);
+            System.out.println("Server is ready to accept connections....");
+            System.out.println("Server is waiting......");
+            socket = server.accept();
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+            startReading();
+            startWriting();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startReading(){
+
+        Runnable r1 = ()->{
+            System.out.println("Reading started...");
+        try {
+            while (true) {
+                    String msg = br.readLine();
+                if(msg.equals("exit")){
+                    System.out.println("Client turminated the chat");
+                    socket.close();
+                    break;
+                }
+
+                System.out.println("Clinet: "+msg);
+                } 
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+
+        new Thread(r1).start();
+
+    }
+
+    public void startWriting(){
+        Runnable r2=()->{
+            System.out.println("Writting started...");
+        try {
+
+            while (true) {
+                    BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+                    String content = br1.readLine();
+                    out.println(content);
+                    out.flush();
+                    if(content.equals("exit")){
+                        socket.close();
+                        break;
+                    }
+                }
+        } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+        new Thread(r2).start();
+    }
+ 
+
+    public static void main(String[] args){
+        System.out.println("This is server..... Going to start server");
+        new Server();
+    }
+}
